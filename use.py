99d056d3762a24fff5b81d2c67fc32310a42869a
zhenglianchi <77715067+zhenglianchi@users.noreply.github.com>
2025-08-11 18:23:47 +0800

    Add files via upload

---
+++ use.py
@@ -0,0 +1,20 @@
+from OmniPicker import OmniPicker_Interface
+import time
+
+print('''The converter is equipped with two built - in conversion protocols, \n
+      one is a fixed 20 byte protocol and the other is a variable length protocol. \n
+      Please ensure that the variable length protocol is selected in the supporting software \n
+      and click the Set and Start button to issue a configuration command to the converter''')
+
+test = OmniPicker_Interface()
+test.connect()
+time.sleep(3)
+test.gripper_half_open()
+time.sleep(3)
+test.gripper_close()
+time.sleep(3)
+test.gripper_open()
+time.sleep(3)
+test.gripper_close()
+time.sleep(3)
+test.disconnect()
